1f529af577c823a9f13146985e034adc4c7e6c56
kuribko <konstantin.kuribko@gmail.com>
2016-03-24 19:14:53 +0200

    different colors for types

--- js/controllers/PokemonController.js
+++ js/controllers/PokemonController.js
@@ -5,4 +5,8 @@ app.controller('PokemonController', ['$scope', 'pokemons', function($scope, poke
 
   $scope.test = "Angular works"
   
+  $scope.typeColor = function(typeName){
+      var colors = ['#FCCB7C', '#7CFC8B', '#FC7CED', '#7CADFC'];
+      return colors[typeName.length % 4];
+  }
 }]);
