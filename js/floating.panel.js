aab8659778195871de8dbaced82dd6065c5ba662
kuribko <konstantin.kuribko@gmail.com>
2016-03-25 14:13:49 +0200

    Refactor floating panel

--- js/floating.panel.js
+++ js/floating.panel.js
@@ -1,21 +1,20 @@
 var floatingPanel = "#right-part";
 var parent = "#main";
+var offsetFromBottom = 100;
 
 $(document).ready(function () {
-    topPanel = parseInt($(floatingPanel).css("top"));//.substring(0,$(name).css("top").indexOf("px")));
+    panelTop = parseInt($(floatingPanel).css("top"));//.substring(0,$(name).css("top").indexOf("px")));
     $(window).scroll(function () {
-        var newPanelTop = topPanel + $(document).scrollTop();
+        var newPanelTop = panelTop + $(document).scrollTop();
         var panelHeight = parseInt($(floatingPanel).css("height"));
-        if ($(document).height() > (newPanelTop + panelHeight + 100)) {
+        if ($(document).height() > (newPanelTop + panelHeight + offsetFromBottom)) {
             $(floatingPanel).animate({top: newPanelTop + "px"}, {duration: 200, queue: false});
         }
     });
 
     var adaptRight = function () {
         var panelRight = $(document).width() - (parseInt($('#content').offset().left) + parseInt($('#content').css('width'))) + "px";
-        console.log(panelRight);
         $(floatingPanel).css({'right': panelRight});
-
     }
 
     adaptRight();
