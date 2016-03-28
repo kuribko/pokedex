571689dcf2d4b903949a2816f29fb4b2ca4606e5
kuribko <konstantin.kuribko@gmail.com>
2016-03-28 20:10:35 +0300

    comma separated types in details

--- js/controllers/PokemonController.js
+++ js/controllers/PokemonController.js
@@ -1,31 +1,31 @@
 app.controller('PokemonController', ['$scope', 'pokemonsService', '$http', function ($scope, pokemonsService, $http) {
-//  pokemonsService.success(function(data){
-//    $scope.pokemons = data;
-//  });
+  pokemonsService.success(function(data){
+    $scope.pokemons = data;
+  });
 
-        $scope.pokemons = {objects: [
-                {name: 'Pokemon_1', pkdx_id: 1, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_2', pkdx_id: 2, types: [{name: 'type_2222'}]},
-                {name: 'Pokemon_3', pkdx_id: 3, types: [{name: 'type_33'}]},
-                {name: 'Pokemon_4', pkdx_id: 4, types: [{name: 'type_4'}, {name: 'type_33'}]},
-                {name: 'Pokemon_5', pkdx_id: 5, types: [{name: 'type_1'}, {name: 'type_555'}]},
-                {name: 'Pokemon_6', pkdx_id: 6, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_7', pkdx_id: 7, types: [{name: 'type_1'}, {name: 'type_33'}]},
-                {name: 'Pokemon_8', pkdx_id: 8, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_9', pkdx_id: 9, types: [{name: 'type_555'}]},
-                {name: 'Pokemon_10', pkdx_id: 10, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_11', pkdx_id: 11, types: [{name: 'type_2'}]},
-                {name: 'Pokemon_12', pkdx_id: 12, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_13', pkdx_id: 13, types: [{name: 'type_33'}]},
-                {name: 'Pokemon_14', pkdx_id: 14, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_15', pkdx_id: 15, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_16', pkdx_id: 16, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_17', pkdx_id: 17, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_18', pkdx_id: 18, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_19', pkdx_id: 19, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_20', pkdx_id: 20, types: [{name: 'type_1'}]},
-                {name: 'Pokemon_21', pkdx_id: 21, types: [{name: 'type_1'}]}
-            ]};
+        //$scope.pokemons = {objects: [
+        //        {name: 'Pokemon_1', pkdx_id: 1, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_2', pkdx_id: 2, types: [{name: 'type_2222'}]},
+        //        {name: 'Pokemon_3', pkdx_id: 3, types: [{name: 'type_33'}]},
+        //        {name: 'Pokemon_4', pkdx_id: 4, types: [{name: 'type_4'}, {name: 'type_33'}]},
+        //        {name: 'Pokemon_5', pkdx_id: 5, types: [{name: 'type_1'}, {name: 'type_555'}]},
+        //        {name: 'Pokemon_6', pkdx_id: 6, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_7', pkdx_id: 7, types: [{name: 'type_1'}, {name: 'type_33'}]},
+        //        {name: 'Pokemon_8', pkdx_id: 8, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_9', pkdx_id: 9, types: [{name: 'type_555'}]},
+        //        {name: 'Pokemon_10', pkdx_id: 10, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_11', pkdx_id: 11, types: [{name: 'type_2'}]},
+        //        {name: 'Pokemon_12', pkdx_id: 12, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_13', pkdx_id: 13, types: [{name: 'type_33'}]},
+        //        {name: 'Pokemon_14', pkdx_id: 14, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_15', pkdx_id: 15, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_16', pkdx_id: 16, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_17', pkdx_id: 17, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_18', pkdx_id: 18, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_19', pkdx_id: 19, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_20', pkdx_id: 20, types: [{name: 'type_1'}]},
+        //        {name: 'Pokemon_21', pkdx_id: 21, types: [{name: 'type_1'}]}
+        //    ]};
 
 
         $scope.typeColor = function (typeName) {
--- js/floating.panel.js
+++ js/floating.panel.js
@@ -6,16 +6,11 @@ $(document).ready(function () {
     var panelTop = parseInt($(floatingPanel).css("top"));//.substring(0,$(name).css("top").indexOf("px")));
     $(window).scroll(function () {
         var newPanelTop = panelTop + $(document).scrollTop();
-        var panelHeight = parseInt($(floatingPanel).css("height"));
-        //if ( parseInt($(document).height()) > (newPanelTop + panelHeight + offsetFromBottom)) {
-        //    console.log("height"+parseInt($(document).height()) +' > '+ (newPanelTop +"+"+ panelHeight +"+"+ offsetFromBottom));
-            $(floatingPanel).animate({top: newPanelTop + "px"}, {duration: 200, queue: false});
-        //}
+        $(floatingPanel).animate({top: newPanelTop + "px"}, {duration: 200, queue: false});
     });
 
     var adaptRight = function () {
         var panelRight = $(document).width() - (parseInt($('#content').offset().left) + parseInt($('#content').css('width'))) + "px";
-        console.log("width: "+$(document).width() +'-'+ (parseInt($('#content').offset().left) +'+'+ parseInt($('#content').css('width'))) + "px");
         $(floatingPanel).css({'right': panelRight});
     }
 
