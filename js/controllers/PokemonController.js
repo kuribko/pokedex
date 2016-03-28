f7da03abb64171841c486b3a68ed35089aa9201d
kuribko <konstantin.kuribko@gmail.com>
2016-03-28 12:59:49 +0300

    dynamic panel updated with content

--- js/controllers/PokemonController.js
+++ js/controllers/PokemonController.js
@@ -1,30 +1,30 @@
-app.controller('PokemonController', ['$scope', 'pokemons', function ($scope, pokemons) {
-//  pokemons.success(function(data){
+app.controller('PokemonController', ['$scope', 'pokemonsService', '$http', function ($scope, pokemonsService, $http) {
+//  pokemonsService.success(function(data){
 //    $scope.pokemons = data;
 //  });
 
         $scope.pokemons = {objects: [
-                {name: 'Pokemon_1', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_2', types: [{name: 'type_2222'}]},
-                {name: 'Pokemon_3', types: [{name: 'type_33'}]},
-                {name: 'Pokemon_4', types: [{name: 'type_4'},{name: 'type_33'}]},
-                {name: 'Pokemon_5', types: [{name: 'type_1'},{name: 'type_555'}]},
-                {name: 'Pokemon_6', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_7', types: [{name: 'type_1'},{name: 'type_33'}]},
-                {name: 'Pokemon_8', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_9', types: [{name: 'type_555'}]},
-                {name: 'Pokemon_10', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_11', types: [{name: 'type_2'}]},
-                {name: 'Pokemon_12', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_13', types: [{name: 'type_33'}]},
-                {name: 'Pokemon_14', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_15', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_16', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_17', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_18', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_19', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_20', types: [{name: 'type_1'}]},
-                {name: 'Pokemon_21', types: [{name:'type_1'}]}
+                {name: 'Pokemon_1', pkdx_id: 1, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_2', pkdx_id: 2, types: [{name: 'type_2222'}]},
+                {name: 'Pokemon_3', pkdx_id: 3, types: [{name: 'type_33'}]},
+                {name: 'Pokemon_4', pkdx_id: 4, types: [{name: 'type_4'}, {name: 'type_33'}]},
+                {name: 'Pokemon_5', pkdx_id: 5, types: [{name: 'type_1'}, {name: 'type_555'}]},
+                {name: 'Pokemon_6', pkdx_id: 6, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_7', pkdx_id: 7, types: [{name: 'type_1'}, {name: 'type_33'}]},
+                {name: 'Pokemon_8', pkdx_id: 8, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_9', pkdx_id: 9, types: [{name: 'type_555'}]},
+                {name: 'Pokemon_10', pkdx_id: 10, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_11', pkdx_id: 11, types: [{name: 'type_2'}]},
+                {name: 'Pokemon_12', pkdx_id: 12, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_13', pkdx_id: 13, types: [{name: 'type_33'}]},
+                {name: 'Pokemon_14', pkdx_id: 14, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_15', pkdx_id: 15, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_16', pkdx_id: 16, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_17', pkdx_id: 17, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_18', pkdx_id: 18, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_19', pkdx_id: 19, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_20', pkdx_id: 20, types: [{name: 'type_1'}]},
+                {name: 'Pokemon_21', pkdx_id: 21, types: [{name: 'type_1'}]}
             ]};
 
 
@@ -37,9 +37,22 @@ app.controller('PokemonController', ['$scope', 'pokemons', function ($scope, pok
 //      return 'http://pokeapi.co/media/img/'+id+'.png';
             return 'img/favicon.png';
         }
-        
-        $scope.selectPokemon = function(id){
-            console.log("pokemon selected: "+id);
-            $scope.selectedPokemon = "this is pokemon from controller";
+
+        $scope.selectPokemon = function (id) {
+            console.log("pokemon selected: " + id);
+
+            $http({
+                method: 'GET',
+                url: 'http://pokeapi.co/api/v1/pokemon/' + id
+            }).then(function successCallback(response) {
+                $scope.selectedPokemon = response.data;
+            }, function errorCallback(response) {
+                $scope.selectedPokemon = response;
+            });
+
+//            $scope.selectedPokemon = $scope.pokemons.objects[id];
+            
+            $scope.selectedPokemon.image = $scope.pokemonImage(id);
+
         };
     }]);
--- js/services/pokemonsService.js
+++ js/services/pokemonsService.js
@@ -1,4 +1,4 @@
-app.factory('pokemons', ['$http', function($http){
+app.factory('pokemonsService', ['$http', function($http){
 	return $http.get('http://pokeapi.co/api/v1/pokemon/?limit=12')
 		.success(function(data){
 			return data;
